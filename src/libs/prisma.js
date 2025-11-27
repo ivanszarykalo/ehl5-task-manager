721e6ab8fa2299bf9dac0ef6706376099c495775
Ivan SZ <ivanszarykalo@gmail.com>
2025-11-27 08:30:32 -0300

    fix: use standard prisma import

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,2 +1,11 @@
-import {PrismaClient} from "@prisma/client";
-export const prisma = new PrismaClient ()
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = globalThis
+
+const prisma = globalForPrisma.prisma || new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
+
+export { prisma }
